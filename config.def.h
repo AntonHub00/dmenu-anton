b5434b8b06e9360f709e5de47320ceeaa50b93d9
Antonio Ochoa <antonhub00@gmail.com>
2021-01-03 23:08:34 -0600

    Oceanic Next theme added (colors)

--- config.def.h
+++ config.def.h
@@ -9,11 +9,11 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#1b2b34", "#fac863" },
-	[SchemeNormHighlight] = { "#ec5f67", "#1b2b34" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    [SchemeNorm] = { "#adb5c0", "#1b2b34" },
+    [SchemeSel] = { "#1b2b34", "#6699cc" },"
+    [SchemeSelHighlight] = { "#1b2b34", "#fac863" },
+    [SchemeNormHighlight] = { "#ec5f67", "#1b2b34" },
+    [SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
